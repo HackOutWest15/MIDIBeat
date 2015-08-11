e79711754e84f70e55b99b438c3ccdeb575a2599
hjorthjort <rikard.hjort@gmail.com>
2015-08-11 14:56:17 +0200

    Play method for sample

--- SampleLibrary.js
+++ SampleLibrary.js
@@ -6,3 +6,7 @@ SampleLibrary.prototype.setSample = function(key, value) {
     this.samples[key] = value;
 }
 
+SampleLibrary.prototype.play = function (key) {
+    var audio = new Audio(this.samples[key].link);
+    audio.play();
+}

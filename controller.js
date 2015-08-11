b311d049950400567f646294a85a8041cea2a9ad
hjorthjort <rikard.hjort@gmail.com>
2015-08-11 17:59:47 +0200

    Display name of audiofile

--- controller.js
+++ controller.js
@@ -11,7 +11,8 @@ function init() {
             "<span class='keyController'>" +
             buttonNumber + 
             "<br>" +
-            "<input class=\"chooseButton\" type=\"image\" src=\"./resources/dropbox-button.png\" value=\"Dropbox\" onClick=\"chooseDropbox(" + buttonNumber + ")\"></input><div id='name" + buttonNumber + "'></div>" +
+            "<div id='name" + buttonNumber + "'>&nbsp;</div>" +
+            "<input class=\"chooseButton\" type=\"image\" src=\"./resources/dropbox-button.png\" value=\"Dropbox\" onClick=\"chooseDropbox(" + buttonNumber + ")\"></input>" +
             "<br>" +
             "<input class=\"chooseButton\" type=\"file\" value=\"Upload\" onChange=\"chooseLocal(" + buttonNumber + ")\"></input>";
             "</span>" +
@@ -24,22 +25,31 @@ function chooseDropbox(buttonNumber) {
     var options = {
         success: function(files) {
             sampleLibrary.setSample(buttonNumber, files[0]);
-            document.getElementById("name" + buttonNumber).innerHTML = files[0].name;
+            setName(buttonNumber, files[0].name);
         },
         multiselect: false,
         linkType: "direct",
         extenstions: ['audio'],
     }
-    Dropbox.choose(options);
-    document.getElementById(buttonNumber).getElementsByTagName("input")[1].value = "";
+    Dropbox.choose(options); 
 }
 
 function chooseLocal(buttonNumber) {
     var file = document.getElementById(buttonNumber).getElementsByTagName("input")[1].files[0];
     var value = {};
     value["link"] = window.URL.createObjectURL(file);
-    value["name"] = file.
+    value["name"] = file.name;
     sampleLibrary.setSample(buttonNumber, file);
+    setName(buttonNumber, value["name"]);
+    clearLocal(buttonNumber);
+}
+
+function clearLocal(buttonNumber) {
+    document.getElementById(buttonNumber).getElementsByTagName("input")[1].value = "";
+}
+
+function setName(buttonNumber, name) {
+    document.getElementById("name" + buttonNumber).innerHTML = name;    
 }
 
 function playKey(key) {
